bee9bad6c5a0fc306f7f7011208ac2cce0b462e8
Kurtz <goncalo.correia0606@gmail.com>
2025-04-12 00:39:27 +0100

    Add files via upload

--- SpyID.py
+++ SpyID.py
@@ -1,27 +1,26 @@
 from telethon.sync import TelegramClient
 from telethon.errors import UsernameNotOccupiedError
 
-# Substitua com seu api_id e api_hash do https://my.telegram.org
+# Substitua com seu api_id e api_hash
 api_id = 
 api_hash = ''
 
 with TelegramClient('session_name', api_id, api_hash) as client:
-    username = input("Enter the person's @username: ").strip()
+    while True:
+        username = input("\nEnter the person's @username: ").strip()
 
-    # Remove o @ se tiver
-    if username.startswith('@'):
-        username = username[1:]
+        # Remove o @ se tiver
+        if username.startswith('@'):
+            username = username[1:]
 
-    try:
-        user = client.get_entity(username)
-        print("\n--- Result ---")
-        print(f"Username: @{user.username}")
-        print(f"Name: {user.first_name}")
-        print(f"ID: {user.id}")
-        print("------------------")
-    except UsernameNotOccupiedError:
-        print(f"\n❌ Ninguém está usando o username @{username}")
-    except Exception as e:
-        print(f"\n⚠️ Ocorreu um erro inesperado:\n{e}")
-
-    input("\nPressione ENTER para sair...")
+        try:
+            user = client.get_entity(username)
+            print("\n--- Result ---")
+            print(f"Username: @{user.username}")
+            print(f"Nome: {user.first_name}")
+            print(f"ID: {user.id}")
+            print("------------------")
+        except UsernameNotOccupiedError:
+            print(f"\n❌ Ninguém está usando o username @{username}")
+        except Exception as e:
+            print(f"\n⚠️ Ocorreu um erro inesperado:\n{e}")
